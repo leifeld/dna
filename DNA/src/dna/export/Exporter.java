1b91cc63a12bac7f5ab9b4b5a68e52f78117f213
leifeld <philip.leifeld@glasgow.ac.uk>
2017-01-22 07:57:47 +0000

    Added further GUI elements to Exporter and started adding tooltips

--- DNA/src/dna/export/Exporter.java
+++ DNA/src/dna/export/Exporter.java
@@ -2,9 +2,12 @@ package dna.export;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -13,6 +16,8 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -20,6 +25,7 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
+import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SpinnerDateModel;
@@ -36,6 +42,7 @@ import dna.renderer.StatementTypeComboBoxRenderer;
  */
 @SuppressWarnings("serial")
 public class Exporter extends JDialog {
+	JCheckBox helpBox;
 	
 	/**
 	 * Opens an Exporter window, which displays the GUI for exporting network data.
@@ -61,20 +68,38 @@ public class Exporter extends JDialog {
 		
 		// first row of options
 		JLabel networkModesLabel = new JLabel("Type of network");
+		String networkModesToolTip = "<html><p width=\"500\">Select the type of network to export. A <strong>one-mode "
+				+ "network</strong> has the same nodes in the rows and columns of the resulting matrix (e.g., "
+				+ "organizations x organizations). A <strong>two-mode network</strong> has different sets of nodes in the "
+				+ "rows and columns of the resulting matrix (e.g., organizations x concepts). An <strong>event list</strong> "
+				+ "is a time-stamped list of edges, with each row of the resulting table containing all variables of a "
+				+ "statement including the time.</p></html>";
+		networkModesLabel.setToolTipText(networkModesToolTip);
 		settingsPanel.add(networkModesLabel, gbc);
 		
 		gbc.gridx = 1;
 		JLabel statementTypeLabel = new JLabel("Statement type");
+		String statementTypeToolTip = 
+				"<html><p width=\"500\">A network is constructed based on the variables from a specific statement type. "
+				+ "Select here which statement type to use.</p></html>";
+		statementTypeLabel.setToolTipText(statementTypeToolTip);
 		settingsPanel.add(statementTypeLabel, gbc);
 		
 		gbc.gridx = 2;
 		JLabel fileFormatLabel = new JLabel("File format");
+		String fileFormatToolTip = "<html><p width=\"500\">Select the file format in which the resulting network is saved. "
+				+ "<strong>CSV</strong> files can be read by most programs, including spreadsheet software like Microsoft "
+				+ "Excel. <strong>DL</strong> files are plain text files which can be imported by the network analysis "
+				+ "software Ucinet. <strong>GRAPHML</strong> files can be opened using the network visualization software "
+				+ "visone. Note that the flavor of GRAPHML used here is specific to visone.</p></html>";
+		fileFormatLabel.setToolTipText(fileFormatToolTip);
 		settingsPanel.add(fileFormatLabel, gbc);
 		
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		String[] networkModesItems = new String[] {"Two-mode network", "One-mode network", "Event list"};
 		JComboBox<String> networkModesBox = new JComboBox<>(networkModesItems);
+		networkModesBox.setToolTipText(networkModesToolTip);
 		settingsPanel.add(networkModesBox, gbc);
 
 		gbc.gridx = 1;
@@ -82,6 +107,7 @@ public class Exporter extends JDialog {
 		StatementTypeComboBoxModel model = new StatementTypeComboBoxModel();
 		JComboBox<StatementType> statementTypeBox = new JComboBox<>(model);
 		statementTypeBox.setRenderer(cbrenderer);
+		statementTypeBox.setToolTipText(statementTypeToolTip);
 		
 		String[] var1Items = null, var2Items = null;
 		for (int i = 0; i < Dna.data.getStatementTypes().size(); i++) {
@@ -106,6 +132,7 @@ public class Exporter extends JDialog {
 		gbc.gridx = 2;
 		String[] fileFormatItems = new String[] {".csv", ".dl", ".graphml"};
 		JComboBox<String> fileFormatBox = new JComboBox<>(fileFormatItems);
+		fileFormatBox.setToolTipText(fileFormatToolTip);
 		settingsPanel.add(fileFormatBox, gbc);
 		fileFormatBox.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		statementTypeBox.setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -116,30 +143,85 @@ public class Exporter extends JDialog {
 		gbc.gridx = 0;
 		gbc.gridy = 2;
 		JLabel var1Label = new JLabel("Variable 1");
+
+		String var1ToolTip = "<html><p width=\"500\">In a one-mode network, the first variable denotes the node class used "
+				+ "both for the rows and columns of the matrix. For example, select the variable for organizations in order "
+				+ "to export an organization x organization network. In a two-mode network, the first variable denotes the "
+				+ "node class for the rows. For example, select the variable for organizations here if you want to export "
+				+ "an organization x concept two-mode network.</p></html>";
+		var1Label.setToolTipText(var1ToolTip);
 		settingsPanel.add(var1Label, gbc);
 		
 		gbc.gridx = 1;
 		JLabel var2Label = new JLabel("Variable 2");
+		String var2ToolTip = "<html><p width=\"500\">In a one-mode network, the second variable denotes the variable through "
+				+ "which the edges are aggregated. For example, if you export a one-mode network of organizations and the "
+				+ "edge weight that connects any two organizations should denote the two organizations' number of joint "
+				+ "concepts, then the second variable should denote the concepts. In a two-mode network, the second variable "
+				+ "denotes the node class used for the columns of the resulting network matrix. For example, one would select "
+				+ "the variable for concepts here in order to export an organization x concept network.</p></html>";
+		var2Label.setToolTipText(var2ToolTip);
 		settingsPanel.add(var2Label, gbc);
 
 		gbc.gridx = 2;
 		JLabel qualifierLabel = new JLabel("Qualifier");
+		String qualifierToolTip = "<html><p width=\"500\">The qualifier is a binary or integer variable which indicates "
+				+ "different qualities or levels of association between variable 1 and variable 2. If a binary qualifier "
+				+ "variable is used, this amounts to positive versus negative relations or agreement versus disagreement. "
+				+ "For example, in an organization x concept two-mode network, a qualifier variable could indicate whether "
+				+ "an organization supports or rejects a concept. The qualifier would indicate support or rejection in this "
+				+ "case. The qualifier is also preserved in a one-mode network. For example, in an organization x organization "
+				+ "network, any two organizations can be connected if they either co-support or co-reject the same concept. "
+				+ "In other words, a one-mode projection of the support two-mode network and a one-mode projection of the "
+				+ "rejection two-mode network are stacked and added in order to obtain a congruence network. If an integer "
+				+ "(non-binary) qualifier variable is used, this indicates different intensities in a two-mode network, for "
+				+ "example the extent of support (in the positive range) or rejection (in the negative range) of a concept by "
+				+ "an organization. In a one-mode network, differences in these levels of intensity are used as edge weights; "
+				+ "the description under Qualifier aggregation contains more details on this.</p></html>";
+		qualifierLabel.setToolTipText(qualifierToolTip);
 		settingsPanel.add(qualifierLabel, gbc);
 
 		gbc.gridx = 3;
 		JLabel aggregationLabel = new JLabel("Qualifier aggregation");
+		String aggregationToolTip = "<html><p width=\"500\">The choices differ between one-mode and two-mode networks. <strong>"
+				+ "ignore</strong> is available in both cases and means that the agreement qualifier variable is ignored, i.e., "
+				+ "the network is constructed as if all values on the qualifier variable were the same. In the two-mode network "
+				+ "case, <strong>subtract</strong> means that negative values on the qualifier variable are subtracted from "
+				+ "positive values (if an integer qualifier is selected) or that 0 values are subtracted from 1 values (if a "
+				+ "binary qualifier variable is selected). For example, if an organization mentions a concept two times in a "
+				+ "positive way and three times in a negative way, there will be an edge weight of -1 between the organization "
+				+ "and the concept. In the one-mode case, <strong>subtract</strong> means that a congruence network and a "
+				+ "conflict network are created separately and then the conflict network ties are subtracted from the congruence "
+				+ "network ties. For example, if two organizations A and B disagree over 3 concepts and agree with regard to 5 "
+				+ "concepts, they have an edge weight of 2. <strong>combine</strong> is only available for two-mode networks. "
+				+ "In contrast to the subtract option, it means that the values of the qualifier are treated as qualitative "
+				+ "categories. In this case, DNA creates all possible combinations of edges (e.g., in the binary case, these are "
+				+ "support, rejection, and both/mixed/ambiguous). Integer values are then used as edge weights, as in a multiplex "
+				+ "network. E.g., 1 represents support, 2 represents rejection, and 3 represents both. With an integer variable, "
+				+ "this may become more complex. In one-mode networks, <strong>congruence</strong> means that similarity or matches "
+				+ "on the qualifier variable are counted in order to construct an edge. With a binary variable, matches are used. "
+				+ "For example, if organizations A and B both co-support or both co-reject concept C, they are connected, and the "
+				+ "number of co-supported and co-rejected concepts is used as the edge weight. With an integer qualifier variable, "
+				+ "the inverse of the absolute distance plus one (i.e., the proximity) is used instead of a match. In one-mode "
+				+ "networks, <strong>conflict</strong> constructs the network by recording disagreements between actors, rather "
+				+ "than agreements, as in the congruence case. With a binary qualifier, this means that organizations A and B "
+				+ "are connected if one of them supports a concept and the other one rejects the concept. With an integer qualifier, "
+				+ "the absolute distance is used instead.</p></html>";
+		aggregationLabel.setToolTipText(aggregationToolTip);
 		settingsPanel.add(aggregationLabel, gbc);
 
 		gbc.insets = new Insets(3, 3, 3, 3);
 		gbc.gridx = 0;
 		gbc.gridy = 3;
 		JComboBox<String> var1Box = new JComboBox<>(var1Items);
+		var1Box.setToolTipText(var1ToolTip);
 		settingsPanel.add(var1Box, gbc);
 		int HEIGHT2 = (int) var1Box.getPreferredSize().getHeight();
 		var1Box.setPreferredSize(new Dimension(WIDTH, HEIGHT2));
 		
 		gbc.gridx = 1;
 		JComboBox<String> var2Box = new JComboBox<>(var2Items);
+		var2Box.setToolTipText(var2ToolTip);
 		var2Box.setSelectedIndex(1);
 		settingsPanel.add(var2Box, gbc);
 		var2Box.setPreferredSize(new Dimension(WIDTH, HEIGHT2));
@@ -148,6 +230,7 @@ public class Exporter extends JDialog {
 		//String[] qualifierItems = getVariablesList((StatementType) statementTypeBox.getSelectedItem(), false, false, true, true);
 		String[] qualifierItems = new String[0];
 		JComboBox<String> qualifierBox = new JComboBox<>(qualifierItems);
+		qualifierBox.setToolTipText(qualifierToolTip);
 		settingsPanel.add(qualifierBox, gbc);
 		qualifierBox.setEnabled(false);
 		qualifierBox.setPreferredSize(new Dimension(WIDTH, HEIGHT2));
@@ -155,6 +238,7 @@ public class Exporter extends JDialog {
 		gbc.gridx = 3;
 		String[] aggregationItems = new String[] {"ignore", "combine", "subtract"};
 		JComboBox<String> aggregationBox = new JComboBox<>(aggregationItems);
+		aggregationBox.setToolTipText(aggregationToolTip);
 		settingsPanel.add(aggregationBox, gbc);
 		aggregationBox.setPreferredSize(new Dimension(WIDTH, HEIGHT2));
 		
@@ -254,7 +338,7 @@ public class Exporter extends JDialog {
 		settingsPanel.add(excludeValuesLabel, gbc);
 		
 		gbc.gridx = 2;
-		JLabel excludePreviewLabel = new JLabel("Preview");
+		JLabel excludePreviewLabel = new JLabel("Preview of excluded values");
 		settingsPanel.add(excludePreviewLabel, gbc);
 		
 		gbc.insets = new Insets(3, 3, 3, 3);
@@ -279,10 +363,56 @@ public class Exporter extends JDialog {
 		settingsPanel.add(excludeValueScroller, gbc);
 		excludeValueScroller.setPreferredSize(new Dimension(WIDTH, (int) excludeValueScroller.getPreferredSize().getHeight()));
 		
+		gbc.gridx = 2;
+		gbc.gridwidth = 2;
+		JTextArea excludePreviewArea = new JTextArea();
+		excludePreviewArea.setPreferredSize(excludeValueScroller.getPreferredSize());
+		excludePreviewArea.setText("This is a test.");
+		excludePreviewArea.setEditable(false);
+		excludePreviewArea.setBackground(settingsPanel.getBackground());
+		JScrollPane excludePreviewScroller = new JScrollPane(excludePreviewArea);
+		settingsPanel.add(excludePreviewScroller, gbc);
+		
+		// sixth row: buttons
+		gbc.gridwidth = 2;
+		gbc.gridx = 0;
+		gbc.gridy = 10;
+		helpBox = new JCheckBox("Display tooltips with instructions");
+		helpBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				toggleHelp();
+			}
+			
+		});
+		settingsPanel.add(helpBox, gbc);
+		
+		gbc.gridx = 2;
+		gbc.anchor = GridBagConstraints.EAST;
+		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		JButton revertButton = new JButton("Revert", new ImageIcon(getClass().getResource("/icons/arrow_rotate_anticlockwise.png")));
+		buttonPanel.add(revertButton);
+		JButton cancelButton = new JButton("Cancel", new ImageIcon(getClass().getResource("/icons/cancel.png")));
+		buttonPanel.add(cancelButton);
+		JButton exportButton = new JButton("Export...", new ImageIcon(getClass().getResource("/icons/accept.png")));
+		buttonPanel.add(exportButton);
+		toggleHelp();
+		settingsPanel.add(buttonPanel, gbc);
+		
 		this.add(settingsPanel, BorderLayout.NORTH);
 		this.pack();
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
+	}
+	
+	public void toggleHelp() {
+		javax.swing.ToolTipManager.sharedInstance().setInitialDelay(10);
+		if (helpBox.isSelected()) {
+			javax.swing.ToolTipManager.sharedInstance().setEnabled(true);
+			javax.swing.ToolTipManager.sharedInstance().setDismissDelay(30000);
+		} else {
+			javax.swing.ToolTipManager.sharedInstance().setEnabled(false);
+			javax.swing.ToolTipManager.sharedInstance().setDismissDelay(0);
+		}
 	}
 	
 	/**
