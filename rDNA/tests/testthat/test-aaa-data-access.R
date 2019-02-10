955cc42170d324df76414db5d918d5a6cc105484
Johannes Gruber <j.gruber.1@research.gla.ac.uk>
2019-02-10 14:27:46 +0000

    Fixed tests when MAKE_DNA is not set (#148)

--- rDNA/tests/testthat/test-aaa-data-access.R
+++ rDNA/tests/testthat/test-aaa-data-access.R
@@ -5,7 +5,8 @@ setup(
 )
 
 # set Sys.setenv(MAKE_DNA = "TRUE") to run this part
-if (Sys.getenv("MAKE_DNA") == "TRUE") {
+if (Sys.getenv("MAKE_DNA") == "TRUE" & 
+    tolower(Sys.getenv("NOT_CRAN")) %in% c("1", "yes", "true")) {
   test_that("download Jar", {
     expect_that({
       file <- dna_downloadJar(path = ".", returnString = TRUE)
@@ -25,11 +26,10 @@ if (Sys.getenv("MAKE_DNA") == "TRUE") {
       )
     },  equals(TRUE))
   })
-} else if (tolower(Sys.getenv("NOT_CRAN")) %in% c("1", "yes", "true") &
-           !nchar(Sys.getenv("TRAVIS_R_VERSION")) > 0) {
+} else if (tolower(Sys.getenv("NOT_CRAN")) %in% c("1", "yes", "true")) {
   test_that("download Jar", {
     expect_that({
-      file <- dna_downloadJar(returnString = TRUE)
+      file <- dna_downloadJar("../../inst/extdata/", returnString = TRUE)
       file.exists(file)
     },  equals(TRUE))
   })
