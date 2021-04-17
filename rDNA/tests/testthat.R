a58b96776b81e086614c5c16b40536a545579698
leifeld <philip.leifeld@essex.ac.uk>
2021-04-17 20:36:46 +0100

    Fixing additional check notes and warnings

--- rDNA/tests/testthat.R
+++ rDNA/tests/testthat.R
@@ -1,14 +1,4 @@
 library("testthat")
 library("rDNA")
 
-if (identical(Sys.getenv("NOT_CRAN"), "true")) {
-  test_check("rDNA")
-} else {
-  jar <- list.files(system.file("extdata", package = "rDNA"),
-                    pattern = ".jar$", full.names = TRUE)
-  if (length(jar) == 0) {
-    jar <- dna_downloadJar(path = ".")
-  }
-  dna_init(jar)
-  test_check("rDNA")
-}
+test_check("rDNA")
