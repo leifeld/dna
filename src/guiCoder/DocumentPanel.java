3ccc27c9094f278b269d16b3ccf9320ef0bc3d97
leifeld <philip.leifeld@essex.ac.uk>
2021-07-24 12:56:39 +0100

    Improved SQL table generation

--- src/guiCoder/DocumentPanel.java
+++ src/guiCoder/DocumentPanel.java
@@ -10,8 +10,6 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
