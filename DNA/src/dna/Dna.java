2d3ce5e6db76aec5b3a81cbffd84005bed7c796d
leifeld <philip.leifeld@essex.ac.uk>
2022-02-07 19:32:56 +0000

    Updated version date before sending test version out to beta testers

--- DNA/src/dna/Dna.java
+++ DNA/src/dna/Dna.java
@@ -14,7 +14,7 @@ public class Dna {
 	public static Dna dna;
 	public static Logger logger;
 	public static Sql sql;
-	public static final String date = "2022-02-02";
+	public static final String date = "2022-02-07";
 	public static final String version = "3.0.1";
 	MainWindow mainWindow;
 	
