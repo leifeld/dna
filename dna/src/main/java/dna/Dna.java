30c828ff50b16dc8ad94651b18d1d3d9c9205c30
leifeld <philip.leifeld@essex.ac.uk>
2022-05-04 15:18:57 +0100

    Made SQL column checks explicit when multiple columns are involved

--- dna/src/main/java/dna/Dna.java
+++ dna/src/main/java/dna/Dna.java
@@ -14,7 +14,7 @@ public class Dna {
 	public static Dna dna;
 	public static Logger logger;
 	public static Sql sql;
-	public static final String date = "2022-05-02";
+	public static final String date = "2022-05-04";
 	public static final String version = "3.0.5";
 	MainWindow mainWindow;
 	
